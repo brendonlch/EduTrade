892bbcbfa5563580c462ad5bab26cce907bdcc2b
BrydonSeah <brydonseah@users.noreply.github.com>
2020-03-11 16:23:38 +0800

    Revert "News completed"
    This reverts commit ab15ff3939f0e218f6bd741af6ad3b7a5e438aff.

--- news/news.py
+++ news/news.py
@@ -1,19 +1,12 @@
 import requests
 import json
-from flask import Flask, request, json
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/news'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 CORS(app)
-#FOR DEBUGGING - eprint()
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
-#hardcoded data
 data = {
   "meta_data": {
     "api_name": "stock_news_v2", 
@@ -1084,60 +1077,35 @@ data = {
   }
 }
 
-class NewsObj(db.Model):
-      __tablename__ = 'news'
+class NewsObj():
+       def __init__(self, author, desc, published, ticker, title, url_article): #Initialise the objects
+        self.author = author
+        self.desc = desc
+        self.published = published
+        self.ticker = ticker
+        self.title = title
+        self.url_article = url_article
 
-      id = db.Column(db.Integer, primary_key=True)
-      title = db.Column(db.String(248), nullable=False)
-      author = db.Column(db.String(248), nullable=False)
-      description = db.Column(db.String(2048), nullable=False)
-      published = db.Column(db.String(1000), nullable=False)
-      ticker = db.Column(db.String(100), nullable=False)
-      url = db.Column(db.String(2048), nullable=False)
-
-      def __init__(self, id, title, author, description, published, ticker, url): #Initialise the objects
-       self.id = id
-       self.title = title
-       self.author = author
-       self.description = description
-       self.published = published
-       self.ticker = ticker
-       self.url = url
-
-      def json(self): 
-        return {"id": self.id, "title": self.title, "author": self.author, "description": self.description, "published": self.published, "ticker": self.ticker,"url": self.url}
-
+       def json(self): 
+        return {"author": self.author, "desc": self.desc, "published": self.published, "ticker": self.ticker
+            , "title": self.title, "url_article": self.url_article}
 
 @app.route("/")
 def get_news_by_tickers():
-  new_data = data['result_data']
-  count = 0
-  new_dict = {}
-  news_objects = {}
-  obj_of_news = {}
+       new_data = data['result_data']
+       count = 0
+       new_dict = {}
+       news_objects = {}
+       for data1 in new_data['GOOGL']:
+              count += 1
+              new_dict[count] = data1
 
-  for data1 in new_data['GOOGL']:
-    count += 1
-    new_dict[count] = data1
-    for i in range (1 , len(new_dict) + 1):
-      # if new_dict[i]['author'] == "":
-      #   x = NewsObj(i, new_dict[i]['title'],"No author", new_dict[i]['description'], new_dict[i]['published'], new_dict[i]['ticker'], new_dict[i]['url'])
-      #   news_objects[i] = x.json()
-      # else:
-        y = NewsObj(i, new_dict[i]['title'], new_dict[i]['author'], new_dict[i]['description'], new_dict[i]['published'], new_dict[i]['ticker'], new_dict[i]['url'])
-        news_objects[i] = y.json()
-  # return news_objects
-  try:
-    for i in range (1, len(news_objects) + 1):
-      #return news_objects
-      # return(news_objects)
-      bird = news_objects[i]
-      obj = NewsObj(**bird)
-      # return obj     
-      db.session.add(obj)
-      db.session.commit()
-  except:
-    return "FAIL"
-  return "Success"
-  
+       for i in range (1 , len(new_dict) + 1):
+              if new_dict[i]['author'] == "":
+                     x = NewsObj("No author", new_dict[i]['description'], new_dict[i]['published'], new_dict[i]['ticker'], new_dict[i]['title'], new_dict[i]['url'])
+                     news_objects[i] = x.json()
+              else:
+                     x = NewsObj(new_dict[i]['author'], new_dict[i]['description'], new_dict[i]['published'], new_dict[i]['ticker'], new_dict[i]['title'], new_dict[i]['url'])
+                     news_objects[i] = x.json()
+       return news_objects
 app.run(port=6002, debug=True)
